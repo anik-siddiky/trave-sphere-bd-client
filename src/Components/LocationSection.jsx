46744b9199d3659aa8ac1c1218b78e31b990b10c
Anik Siddiky <codesbynik@gmail.com>
2025-08-10 19:21:13 +0600

    fixed the location comp

--- src/Components/LocationSection.jsx
+++ src/Components/LocationSection.jsx
@@ -14,8 +14,7 @@ const LocationSection = () => {
                         loading="lazy"
                         allowFullScreen
                         className="border-0 w-full h-full"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.9034943956433!2d90.39104997584263!3d23.750876178688392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c78fd78c32cd%3A0x7b39d158e7a5103e!2sDhaka%2C%20Bangladesh!5e0!3m2!1sen!2sus!4v1718370720000!5m2!1sen!2sus"
-                    ></iframe>
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.9034943956433!2d90.39104997584263!3d23.750876178688392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c78fd78c32cd%3A0x7b39d158e7a5103e!2sDhaka%2C%20Bangladesh!5e0!3m2!1sen!2sus!4v1718370720000!5m2!1sen!2sus"></iframe>
                 </div>
             </div>
         </section>
